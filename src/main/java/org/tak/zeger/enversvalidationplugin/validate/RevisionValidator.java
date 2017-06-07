901726f68a1fbf07e107dccbd4e8452468d4bad2
Zeger Tak <Zeger.Tak@topicus.nl>
2017-06-07 09:30:34 +0200

    Merge 2 content phase validators together which use the same data

--- src/main/java/org/tak/zeger/enversvalidationplugin/validate/RevisionValidator.java
+++ src/main/java/org/tak/zeger/enversvalidationplugin/validate/RevisionValidator.java
@@ -3,11 +3,15 @@ package org.tak.zeger.enversvalidationplugin.validate;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.dbunit.database.CachedResultSetTable;
 import org.dbunit.dataset.DataSetException;
 import org.tak.zeger.enversvalidationplugin.annotation.ConnectionProvider;
@@ -23,13 +27,13 @@ import org.tak.zeger.enversvalidationplugin.exceptions.ValidationException;
 import org.tak.zeger.enversvalidationplugin.utils.DatabaseUtils;
 
 @ValidationType(TargetPhase.CONTENT)
-public class AddOrUpdateRevisionExistForAuditableRecordsValidator extends AbstractValidator
+public class RevisionValidator extends AbstractValidator
 {
 	private final String auditedTableName;
 	private final Map<String, TableRow> recordsInAuditedTableIdentifiedByPK;
 	private final Map<String, List<TableRow>> recordsInAuditTable;
 
-	public AddOrUpdateRevisionExistForAuditableRecordsValidator(@Nonnull String auditedTableName, @Nonnull Map<String, TableRow> recordsInAuditedTableIdentifiedByPK, @Nonnull Map<String, List<TableRow>> recordsInAuditTable)
+	public RevisionValidator(@Nonnull String auditedTableName, @Nonnull Map<String, TableRow> recordsInAuditedTableIdentifiedByPK, @Nonnull Map<String, List<TableRow>> recordsInAuditTable)
 	{
 		this.auditedTableName = auditedTableName;
 		this.recordsInAuditedTableIdentifiedByPK = recordsInAuditedTableIdentifiedByPK;
@@ -80,6 +84,65 @@ public class AddOrUpdateRevisionExistForAuditableRecordsValidator extends Abstra
 		if (!identifiersWhichShouldHaveAnAddOrUpdateRevision.isEmpty())
 		{
 			throw new ValidationException("The following identifiers " + identifiersWhichShouldHaveAnAddOrUpdateRevision + " in table " + auditedTableName + " do not have an add/update revision table as their last revision or do not have a revision at all");
+		}
+	}
+
+	@Validate
+	public void testAllLatestAddOrRemoveRevisionsShowCurrentStatus()
+	{
+		final Map<String, Map<String, TableRow>> incorrectHistory = new HashMap<>();
+		for (Map.Entry<String, List<TableRow>> auditHistory : recordsInAuditTable.entrySet())
+		{
+			final List<TableRow> auditHistoryValue = auditHistory.getValue();
+			final TableRow lastRecord = auditHistoryValue.get(auditHistoryValue.size() - 1);
+			final Object columnValue = lastRecord.getColumnValue(RevisionConstants.REVTYPE_COLUMN_NAME);
+			if (columnValue == RevisionConstants.DO_NOT_VALIDATE_REVISION)
+			{
+				continue;
+			}
+			final int revType = ((BigDecimal) columnValue).intValue();
+			if (revType != RevisionConstants.ADD_REVISION && revType != RevisionConstants.UPDATE_REVISION)
+			{
+				continue;
+			}
+
+			final TableRow actualRecord = recordsInAuditedTableIdentifiedByPK.get(auditHistory.getKey());
+			if (actualRecord == null)
+			{
+				incorrectHistory.put(auditHistory.getKey(), Collections.emptyMap());
+				continue;
+			}
+
+			final Set<String> columnNames = actualRecord.getColumnNames();
+			final Map<String, TableRow> incorrectColumns = new HashMap<>();
+			for (String columnName : columnNames)
+			{
+				final Object auditedValue = lastRecord.getColumnValue(columnName);
+				final Object actualColumnValue = actualRecord.getColumnValue(columnName);
+
+				final int compare = ObjectUtils.compare((Comparable) actualColumnValue, (Comparable) auditedValue);
+				if (compare != 0)
+				{
+					if (incorrectColumns.isEmpty())
+					{
+						incorrectColumns.put("actual", new TableRow());
+						incorrectColumns.put("audited", new TableRow());
+					}
+
+					incorrectColumns.get("actual").addColumn(columnName, actualColumnValue);
+					incorrectColumns.get("audited").addColumn(columnName, auditedValue);
+				}
+			}
+
+			if (!incorrectColumns.isEmpty())
+			{
+				incorrectHistory.put(auditHistory.getKey(), incorrectColumns);
+			}
+		}
+
+		if (!incorrectHistory.isEmpty())
+		{
+			throw new ValidationException("The latest (add/update) revision for the following identifiers: " + incorrectHistory + " for table " + auditedTableName + " does not reflect the actual values in the table.");
 		}
 	}
 }
